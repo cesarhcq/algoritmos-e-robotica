9e2de13865fd452ed9cbad86ef9517635133f631
cesarhcq <cesarhcq@gmail.com>
2024-08-11 20:56:09 -0300

    ajustado: primeiro helloworld

--- exercicios/hello.cpp
+++ exercicios/hello.cpp
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, soma;
 
-    printf("Digite o primeiro número: ");
-    scanf("%d", &num1);
-
-    printf("Digite o segundo número: ");
-    scanf("%d", &num2);
-
-    soma = num1 + num2;
-
-    printf("A soma de %d e %d é %d.\n", num1, num2, soma);
+    printf("Primeiro exercicio");
 
     return 0;
 }
